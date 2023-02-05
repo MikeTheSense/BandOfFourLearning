e2fb45888f3f8bccaf24e42f91ed4a6b26bc90f4
MikeTheSense <mixail19997@gmail.com>
2023-02-05 22:14:21 +0400

    Added singleton lazy service

---
+++ src/learnpatterns/PropertiesService.java
@@ -0,0 +1,27 @@
+package learnpatterns;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesService {
+    private static PropertiesService propertiesLazyService;
+    private final String PATH = "path";
+    private Properties properties;
+
+    private PropertiesService() {
+        try (FileInputStream fis = new FileInputStream(PATH)) { this.properties.load(fis); }
+        catch (FileNotFoundException e) { System.out.println("File not found"); }
+        catch (IOException e) { System.out.println("Can't read properties from a file"); }
+    }
+
+    public static PropertiesService getInstance(){
+        if (propertiesLazyService == null) propertiesLazyService = new PropertiesService();
+        return propertiesLazyService;
+    }
+
+    public Properties getProperties() {
+        return properties;
+    }
+}
